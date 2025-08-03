98f27da5a66fbf08aa3a05cc137c2dd9b9ba4764
Toskeha22 <toskpock@gmail.com>
2025-08-03 13:29:06 +0300

    accounts app settings

--- chores/Chores/settings.py
+++ chores/Chores/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+
 ]
 
 MIDDLEWARE = [
@@ -110,6 +112,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+AUTH_USER_MODEL = 'accounts.User'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
