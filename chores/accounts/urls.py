e66ee4b3293bbdd4963cd11478ef79782d8c9bac
Toskeha22 <toskpock@gmail.com>
2025-08-29 18:50:18 +0300

    error correction in urls

--- chores/accounts/urls.py
+++ chores/accounts/urls.py
@@ -8,9 +8,9 @@ urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
     path('profile/', views.ProfileView.as_view(), name='profile'),
-    path('profile.setup/', views.ProfileSetupView.as_view(), name='profile setup'),
-    path('profile.edit/', views.ProfileEditView.as_view(), name='profile edit'),
-    path('availability.edit/', views.AvailabilityEditView.as_view(), name='availability edit'),
-    path('change.password/', views.ChangePasswordView.as_view(), name='change password'),
+    path('profile/setup', views.ProfileSetupView.as_view(), name='profile setup'),
+    path('profile/edit', views.ProfileEditView.as_view(), name='profile edit'),
+    path('availability/edit', views.AvailabilityEditView.as_view(), name='availability edit'),
+    path('change/password', views.ChangePasswordView.as_view(), name='change password'),
     path('deleteaccount/', views.DeleteAccountView.as_view(), name='deleteaccount view'),
 ]
