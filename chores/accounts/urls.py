3fd4dc338a131405d5145260c6ada63a45ec5f78
Toskeha22 <toskpock@gmail.com>
2025-08-29 18:27:56 +0300

    update urls

--- chores/accounts/urls.py
+++ chores/accounts/urls.py
@@ -6,5 +6,11 @@ app_name = 'accounts'
 urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
     path('login/', views.LoginView.as_view(), name='login'),
-    path('logout', views.LogoutView.as_view(), name='logout')
+    path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('profile/', views.ProfileView.as_view(), name='profile'),
+    path('profile.setup/', views.ProfileSetupView.as_view(), name='profile setup'),
+    path('profile.edit/', views.ProfileEditView.as_view(), name='profile edit'),
+    path('availability.edit/', views.AvailabilityEditView.as_view(), name='availability edit'),
+    path('change.password/', views.ChangePasswordView.as_view(), name='change password'),
+    path('deleteaccount/', views.DeleteAccountView.as_view(), name='deleteaccount view'),
 ]
