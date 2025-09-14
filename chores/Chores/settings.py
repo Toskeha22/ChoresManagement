821c870d5c63a87f486235976aa081882955d125
Toskeha22 <toskpock@gmail.com>
2025-09-14 13:17:12 +0300

    setup templates and css

--- chores/Chores/settings.py
+++ chores/Chores/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'Chores.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,6 +118,18 @@ AUTH_USER_MODEL = 'accounts.User'
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Папка для збору статичних файлів у продакшені
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Папки, де Django шукає статичні файли під час розробки
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+# Media files (користувацькі завантаження)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
